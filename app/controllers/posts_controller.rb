ff7e4e366715b722346833357eeb3eca71d558bc
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-10-27 12:55:11 -0500

    added post new create in controller, added views new post with error validation
    added rspec context test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class  PostsController < ApplicationController
   def new
     @posts = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+       if @post.save
+         flash[:notice] = 'Post was successfully created.'
+         format.html { redirect_to root_path }
+       else
+         flash[:error] = @post.errors.full_messages
+         format.html { render new_post_path }
+       end
+     end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :description, :link)
+  end
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe Post, type: :model do
   it do
     should validate_length_of(:title).is_at_least(3)
   end
+
+  let(:valid_post) { Post.new(title: 'Hello') }
+  let(:invalid_post) { Post.new(title: '') }
+
+  describe '#validations' do
+    context 'when the post has a valid title' do
+      it 'should be valid' do
+        expect(valid_post.valid?).to be true
+      end
+
+      it 'should save' do
+        expect(valid_post.save).to be true
+      end
+    end
+
+    context 'when the post has an blank title' do
+      it 'should have errors "can\'t be blank"' do
+        invalid_post.valid?
+        expect(invalid_post.errors.full_messages.to_s).to match(/be blank/)
+      end
+    end
+  end
 end
