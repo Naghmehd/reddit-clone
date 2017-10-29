37de023440a596cb58bee0600f5c9f14c251e456
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-10-29 15:39:02 -0500

    added test for create post

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new Post" do
+        expect{
+          post :create, params: { post: { title: 'The Big Bang Theory' } }
+        }.to change{Post.count}.from(0).to(1)
+      end
+      it "redirects to the home page" do
+        post :create, params: { post: { title: 'The Big Bang Theory' } }
+        response.should redirect_to root_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not create a new post " do
+        expect {
+         post :create, params: { post: { title: '' } }
+       }.to_not change{Post.count}
+      end
+    end
+  end
+end
