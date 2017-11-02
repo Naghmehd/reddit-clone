d34bafde4231b468bc863d3e24bc069fef51e688
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-11-01 21:34:00 -0500

    updated before action only for new and added flash error message for failed create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,30 @@
 class  CommentsController < ApplicationController
-  before_action do
-    @post = Post.find(params[:post_id])
-  end
+  before_action :find_post, only: :new
 
-  def index
-    @comments = @post.comments
-  end
   def new
     @comment = @post.comments.new
   end
 
   def create
-    @comment = @post.comments.new(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to post_comments_path(@post)
+      redirect_to root_path
+    else
+      flash[:error] = @comment.errors.full_messages
+      redirect_to new_comment_path(post_id: comment_params[:post_id])
+    end
   end
 
-Private
+private
+
+  def find_post
+    #<PostObject id: 4, title: 'sometime', 'desc', 'link' >
+    @post = Post.find(params[:post_id])
+  end
+
   def comment_params
+    # { comment: { description: 'some description', post_id: 4 } }
     params.require(:comment).permit(:description, :post_id)
   end
+
 end
