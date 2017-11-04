7f5e828ac9622d9d2d10b554833c44650bf6c930
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-11-03 19:41:15 -0500

    added user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:notice] = 'Thank you for signing up!'
+        redirect_to root_path
+      else
+        flash[:error] = @user.errors.full_messages
+        render new
+      end
+  end
+
+private
+
+  def user_params
+    # { email: 'someemail@email.com', password: 'password', 'password_confirmation: 'password' }
+    params.require(:user).permit(:email,
+                                 :password,
+                                 :password_confirmation
+                                 )
+  end
+end
