1cc03ac929d3c090a263cf42476073ec81cd669e
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-11-03 22:06:56 -0500

    added users to post, did migration and added relations to user and post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   # ensure that title is present and at least 3 characters long
  validates :title, length: { minimum: 3 }, presence: true
 
+ belong_to :users
  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
 
   has_secure_password
+  has_many :posts, dependent: :destroy
 
 
-  validates :email, uniqueness: true, on: :create, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :email, uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
   validates :password, length: { in: 6..20 }
 
 end
