f80d39e3b86492e08cd6049e877f629cbdbf980e
naghmehd <naghmehd@gmail.comssh-keygen -t rsa -C naghmehd@gmail.com>
2017-11-03 20:00:20 -0500

    added routes for user and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'posts#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'users', to: 'users#create'
+
+  get 'login',  to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resources :comments
   resources :posts
 
